ab86e03f4aa75cc92db2381668785c55a0d953d4
saikatbhuiyan <shaikotbhuiya@gmail.com>
2023-10-05 17:45:13 +0600

    GameDataParser Flow control by exceptions and Json exceptions

--- 04_GameDataParser/Program.cs
+++ 04_GameDataParser/Program.cs
@@ -1,31 +1,67 @@
-﻿
-using System.Text.Json;
+﻿using System.Text.Json;
 
-Console.WriteLine("Enter the name of the file you want to read:");
-var fileName = Console.ReadLine();
+var isFileReaded = false;
+// string fileContents = default;
+var fileContents = default(string);
+var fileName = default(string);
 
-var fileContents = File.ReadAllText(fileName);
+do
+{
+    try
+    {
+        Console.WriteLine("Enter the name of the file you want to read:");
+        fileName = Console.ReadLine();
+        fileContents = File.ReadAllText(fileName);
+        isFileReaded = true;
+    }
+    catch (ArgumentNullException e)
+    {
+        Console.WriteLine("The file name can not null.");
+    }
+    catch (ArgumentException e)
+    {
+        Console.WriteLine("The file name can not empty.");
+    }
+    catch (FileNotFoundException e)
+    {
+        Console.WriteLine("The file not found.");
+    }
+} while (!isFileReaded);
 
-var videoGames = JsonSerializer.Deserialize<List<VideoGames>>(fileContents);
+
+List<VideoGame> videoGames = default;
+
+try
+{
+    videoGames = JsonSerializer.Deserialize<List<VideoGame>>(fileContents);
+}
+catch (JsonException e)
+{
+    var originalColor = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"JSON in {fileName} file was not " + $"in a valid format. JSON body:");
+    Console.WriteLine(fileContents);
+    Console.ForegroundColor = originalColor;
+    throw new JsonException($"{e.Message} The file is: {fileName}", e);
+}
 
 if (videoGames.Count > 0)
 {
     Console.WriteLine();
     Console.WriteLine("Loaded games are:");
-    foreach (var videoGame in videoGames)
-    {
-        Console.WriteLine(videoGame);
-    }
-    
+    foreach (var videoGame in videoGames) Console.WriteLine(videoGame);
 }
 
 Console.ReadKey();
 
-public class VideoGames
+public class VideoGame
 {
     public string Title { get; init; }
     public int ReleaseYear { get; init; }
     public decimal Rating { get; init; }
 
-    public override string ToString() => $"{Title}, released in {ReleaseYear}, rating: {Rating}";
+    public override string ToString()
+    {
+        return $"{Title}, released in {ReleaseYear}, rating: {Rating}";
+    }
 }
