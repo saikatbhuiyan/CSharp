e3e4760367e88cc8141333a8f111bc35ca5c7a6a
saikatbhuiyan <shaikotbhuiya@gmail.com>
2023-10-05 18:33:47 +0600

    Create custom logger

---
+++ 04_GameDataParser/Logger.cs
@@ -0,0 +1,21 @@
+public class Logger
+{
+    private readonly string _logFileName;
+
+    public Logger(string logFileName)
+    {
+        _logFileName = logFileName;
+    }
+
+    public void Log(Exception e)
+    {
+        var entry = $@"[{DateTime.Now}] 
+Exception message: {e.Message}
+Stack trace: {e.StackTrace}
+
+
+";
+        
+        File.AppendAllText(_logFileName, entry);
+    }
+}
--- 04_GameDataParser/Program.cs
+++ 04_GameDataParser/Program.cs
@@ -1,58 +1,84 @@
 ﻿using System.Text.Json;
 
-var isFileReaded = false;
-// string fileContents = default;
-var fileContents = default(string);
-var fileName = default(string);
-
-do
-{
-    try
-    {
-        Console.WriteLine("Enter the name of the file you want to read:");
-        fileName = Console.ReadLine();
-        fileContents = File.ReadAllText(fileName);
-        isFileReaded = true;
-    }
-    catch (ArgumentNullException e)
-    {
-        Console.WriteLine("The file name can not null.");
-    }
-    catch (ArgumentException e)
-    {
-        Console.WriteLine("The file name can not empty.");
-    }
-    catch (FileNotFoundException e)
-    {
-        Console.WriteLine("The file not found.");
-    }
-} while (!isFileReaded);
-
-
-List<VideoGame> videoGames = default;
+var app = new GameDataParserApp();
+var logger = new Logger("log.txt");
 
 try
 {
-    videoGames = JsonSerializer.Deserialize<List<VideoGame>>(fileContents);
+    app.Run();
 }
-catch (JsonException e)
+catch (Exception e)
 {
-    var originalColor = Console.ForegroundColor;
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine($"JSON in {fileName} file was not " + $"in a valid format. JSON body:");
-    Console.WriteLine(fileContents);
-    Console.ForegroundColor = originalColor;
-    throw new JsonException($"{e.Message} The file is: {fileName}", e);
+    Console.WriteLine(
+        "Sorry! The application has experienced an unexpected error " +
+        "and will have to be closed.");
+    logger.Log(e);
 }
 
-if (videoGames.Count > 0)
+
+public class GameDataParserApp
 {
-    Console.WriteLine();
-    Console.WriteLine("Loaded games are:");
-    foreach (var videoGame in videoGames) Console.WriteLine(videoGame);
+    public void Run()
+    {
+        var isFileReaded = false;
+        // string fileContents = default;
+        var fileContents = default(string);
+        var fileName = default(string);
+        
+        do
+        {
+            try
+            {
+                Console.WriteLine("Enter the name of the file you want to read:");
+                fileName = Console.ReadLine();
+                fileContents = File.ReadAllText(fileName);
+                isFileReaded = true;
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("The file name can not null.");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("The file name can not empty.");
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("The file not found.");
+            }
+        } while (!isFileReaded);
+        
+        
+        List<VideoGame> videoGames = default;
+        
+        try
+        {
+            videoGames = JsonSerializer.Deserialize<List<VideoGame>>(fileContents);
+        }
+        catch (JsonException e)
+        {
+            var originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"JSON in {fileName} file was not " + $"in a valid format. JSON body:");
+            Console.WriteLine(fileContents);
+            Console.ForegroundColor = originalColor;
+            throw new JsonException($"{e.Message} The file is: {fileName}", e);
+        }
+        
+        if (videoGames.Count > 0)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Loaded games are:");
+            foreach (var videoGame in videoGames) Console.WriteLine(videoGame);
+        }
+        
+        
+        Console.ReadKey();
+    }
 }
 
-Console.ReadKey();
+
+
 
 public class VideoGame
 {
