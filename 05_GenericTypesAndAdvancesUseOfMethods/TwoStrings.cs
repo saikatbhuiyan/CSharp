840c1ba844f1132a3214e0c6730018aeecff4de1
saikatbhuiyan <shaikotbhuiya@gmail.com>
2023-10-16 18:33:29 +0600

    05_GenericTypesAndAdvancesUseOfMethods 1

---
+++ 05_GenericTypesAndAdvancesUseOfMethods/TwoStrings.cs
@@ -0,0 +1,13 @@
+namespace _05_GenericTypesAndAdvancesUseOfMethods;
+
+public class TwoStrings
+{
+    public TwoStrings(string string1, string string2)
+    {
+        String1 = string1;
+        String2 = string2;
+    }
+
+    public string String1 { get; }
+    public string String2 { get; }
+}
